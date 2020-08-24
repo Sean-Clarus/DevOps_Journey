1917e4c39fb340cc80afde97d9d0024aaac03e4f
Sean A <serclarus@gmail.com>
2020-08-24 07:19:52 -0400

    Certification study notes included

--- 001 - Python/700 - Python-Udemy-Examples/HelloWorld/conditions.py
+++ 001 - Python/700 - Python-Udemy-Examples/HelloWorld/conditions.py
@@ -1,7 +1,7 @@
 age = int(input("How old are you? "))
 
 # if age >= 16 and age <= 65:
-if 16 <= age <= 65:
+if 16 <= age <= 65: # or we can write like this ==> if age in range(16, 66):
     print("Have a good day at work.")
 else:
     print("Enjoy your free time.")
--- 001 - Python/700 - Python-Udemy-Examples/HelloWorld/guessing_game.py
+++ 001 - Python/700 - Python-Udemy-Examples/HelloWorld/guessing_game.py
@@ -1,20 +1,20 @@
-answer = 5
-
-print("Please guess a number between 1 and 10: ")
-guess = int(input())
-
-if guess != answer:
-    if guess < answer:
-        print("Please guess higher")
-    else:
-        print("Please guess lower")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
-    else:
-        print("Sorry, you have not guessed correctly")
-else:
-    print("you got it first time")
+# answer = 5
+#
+# print("Please guess a number between 1 and 10: ")
+# guess = int(input())
+#
+# if guess != answer:
+#     if guess < answer:
+#         print("Please guess higher")
+#     else:
+#         print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# else:
+#     print("you got it first time")
 
 # if guess < answer:
 #     print("Please guess higher")
@@ -32,3 +32,25 @@ else:
 #         print("Sorry, you have not guessed correctly")
 # else:
 #     print("You got it first time")
+
+import random
+
+highest = 10
+answer = random.randint(1, highest)
+print(answer) # TODO: Remove after testing
+
+print("Please guess a number between 1 and {}: ".format(highest))
+guess = int(input())
+
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
+    else:
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
+else:
+    print("you got it first time")
